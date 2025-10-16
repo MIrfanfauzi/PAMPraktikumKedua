24577c02e74a994397951f2f70d630f3f84dbea9
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-10-16 16:19:23 +0700

    membuat isi text di TataletakRow

--- app/src/main/java/com/example/pampraktikumpertama/Tataletak.kt
+++ app/src/main/java/com/example/pampraktikumpertama/Tataletak.kt
@@ -33,7 +33,13 @@ fun TataletakBox(modifier: modifier) {
         modifier = modifier
             .fillMaxHeight()
             .fillMaxWidth(), contentAlignment = Alignment.Center
-    )
+    ){
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
+    }
 }
 
 
